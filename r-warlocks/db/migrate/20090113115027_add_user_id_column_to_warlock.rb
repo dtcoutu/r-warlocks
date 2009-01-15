b067a46e849d93fa3f9d4a9d55816fe005a1de18
david.coutu <david.coutu@e993bc6e-aa19-11dd-9a07-0f3041a8e97c>
2009-01-15 11:12:58 +0000

    Fixed the joining of users and warlocks which helped clean up the remaining errors.

--- r-warlocks/db/migrate/20090113115027_add_user_id_column_to_warlock.rb
+++ r-warlocks/db/migrate/20090113115027_add_user_id_column_to_warlock.rb
@@ -1,9 +1,9 @@
 class AddUserIdColumnToWarlock < ActiveRecord::Migration
   def self.up
-	add_column :warlocks, :userid, :integer, :null => false
+	add_column :warlocks, :user_id, :integer, :null => false
   end
 
   def self.down
-	remove_column :warlocks, :userid
+	remove_column :warlocks, :user_id
   end
 end
