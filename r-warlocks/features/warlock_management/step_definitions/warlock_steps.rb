37181f3a5a6fa7a1914308c8cd9d90b81b2bc0ab
david.coutu <david.coutu@e993bc6e-aa19-11dd-9a07-0f3041a8e97c>
2009-03-01 01:26:17 +0000

    Added scenario to assert that users can only see their own warlocks.

--- r-warlocks/features/warlock_management/step_definitions/warlock_steps.rb
+++ r-warlocks/features/warlock_management/step_definitions/warlock_steps.rb
@@ -2,9 +2,10 @@ Given /^"(.*)" has "(\d)" warlocks$/ do |username, limit|
 	user = User.find_by_username(username)
 	count = 1
 	while (count <= limit.to_i) do
-		warlock = Warlock.new(:name => 'warlock' + count.to_s, :user_id => user.id)
-		warlock.save
-		count += 1
+		warlock = Warlock.new(:name => username + '-warlock' + count.to_s, :user_id => user.id)
+		if (warlock.save)
+			count += 1
+		end
 	end
 end
 
@@ -29,4 +30,12 @@ end
 
 Then /^I will see the warlock "(.*)" listed on the page$/ do |warlock_name|
 	response.should have_tag("div.warlock-summary", :text => "Rasper")
+end
+
+Then /^I will see "(\d)" warlocks for user "(.*)"$/ do |warlock_count, username|
+	count = 1
+	while (count <= warlock_count.to_i) do
+		response.should have_tag("a", :text => username + "-warlock" + count.to_s)
+		count += 1
+	end
 end
