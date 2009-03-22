1f9625b469f1b4c3ef52e7267b54892c2e0e3f3c
david.coutu <david.coutu@e993bc6e-aa19-11dd-9a07-0f3041a8e97c>
2009-03-22 23:59:50 +0000

    Finished up support of leaving challenges and little refactoring of the logic being used by both the controller and the steps in to the Match model.

--- r-warlocks/features/challenge_management/challenge_joining.feature
+++ r-warlocks/features/challenge_management/challenge_joining.feature
@@ -3,22 +3,22 @@ Feature: Joining an existing challenge
 	I want to be able to join a challenge
 	So that I may play against another warlock
 	
-	Scenario: Joining a match with only one open place left.
+	Scenario: Joining a challenge with only one open place left.
 		Given a warlock named "otherWarlock" exists
 		And a challenge was created by "otherWarlock" for "2" players
 		And I am logged in as "testUser"
 		And I have selected the warlock "testWarlock"
 		And I am on the "/challenge/list_open" page
-		When I click on "Join" for the match created by "otherWarlock"
+		When I click on "Join" for the challenge that "otherWarlock" created
 		Then I will be directed to the "/challenge/list" page
 		And I will see a challenge in the ready list
 	
-	Scenario: Joining a match for which there are other open spots
+	Scenario: Joining a challenge for which there are other open spots
 		Given a warlock named "otherWarlock" exists
 		And a challenge was created by "otherWarlock" for "3" players
 		And I am logged in as "testUser"
 		And I have selected the warlock "testWarlock"
 		And I am on the "/challenge/list_open" page
-		When I click on "Join" for the match created by "otherWarlock"
+		When I click on "Join" for the challenge that "otherWarlock" created
 		Then I will be directed to the "/challenge/list" page
 		And I will see a challenge in the waiting for challenger list
--- r-warlocks/features/challenge_management/step_definitions/challenge_steps.rb
+++ r-warlocks/features/challenge_management/step_definitions/challenge_steps.rb
@@ -7,6 +7,30 @@ Given /^a challenge was created by "(.*)" for "(\d)" players$/ do |warlock, num_
 	match.save
 end
 
+Given /^a challenge was created by "(.*)" for "(\d)" players with "(.*)" invited$/ do |warlock, num_challengers, invited_warlocks_string|
+	Given "a challenge was created by \"#{warlock}\" for \"#{num_challengers}\" players"
+	warlock = Warlock.find_by_name(warlock)
+	match = warlock.matches.last
+	invited_warlocks = invited_warlocks_string.split(", ")
+	for invited_warlock in invited_warlocks
+		Given "a warlock named \"#{invited_warlock}\" exists"
+		current_warlock = Warlock.find_by_name(invited_warlock)
+		match.invited_warlocks << current_warlock
+	end
+end
+
+Given /^a challenge was created by "(.*)" for "(\d)" players with "(.*)" joined$/ do |warlock, num_challengers, joined_warlocks_string|
+	Given "a challenge was created by \"#{warlock}\" for \"#{num_challengers}\" players"
+	warlock = Warlock.find_by_name(warlock)
+	match = warlock.matches.last
+	joined_warlocks = joined_warlocks_string.split(", ")
+	for joined_warlock in joined_warlocks
+		Given "a warlock named \"#{joined_warlock}\" exists"
+		current_warlock = Warlock.find_by_name(joined_warlock)
+		match.warlocks << current_warlock
+	end
+end
+
 When /^I create a challenge for "(\d)" warlocks$/ do |num_challengers|
 	fill_in "match[num_challengers]", :with => num_challengers
 	click_button "Create"
@@ -22,12 +46,22 @@ When /^I create a challenge for "(\d)" warlocks specifying the opponents "(.*)"$
 	click_button "Create"
 end
 
-When /^I click on "(.*)" for the match created by "(.*)"$/ do |click_action, warlock_name|
+When /^I click on "(.*)" for the challenge that "(.*)" created$/ do |click_action, warlock_name|
 	# not sure what the within part should be?
 	#warlock = Warlock.find_by_name(warlock_name)
 	#matches = warlock.matches
 	#click_link_within "#challenge_" + matches[0].id.to_s , "Join"
-	click_link "Join"
+	click_link click_action
+end
+
+When /^I click on "(.*)" for the challenge$/ do |click_action|
+	click_link click_action
+end
+
+When /^"(.*)" leaves the challenge they created$/ do |warlock_name|
+	warlock = Warlock.find_by_name(warlock_name)
+	match = warlock.matches.last
+	match.leave(warlock)
 end
 
 Then /^I will see a challenge in the waiting for challenger list$/ do
@@ -44,4 +78,18 @@ end
 
 Then /^I will see a challenge in the ready list$/ do
 	response.should have_tag("ul.readyMatches")
+end
+
+Then /^the warlock "(.*)" should see "(\d)" open challenges$/ do |warlock_name, num_challenges|
+	warlock = Warlock.find_by_name(warlock_name)
+	open_matches = Match.find_open_for_warlock(warlock)
+	
+	assert_equal num_challenges.to_i, open_matches.size
+end
+
+Then /^the warlock "(.*)" should see "(\d)" pending challenges$/ do |warlock_name, num_challenges|
+	warlock = Warlock.find_by_name(warlock_name)
+	pending_challenges = warlock.matches.find_all_by_status("Open")
+	
+	assert_equal num_challenges.to_i, pending_challenges.size
 end
