66ebea46eea99abf60565e342e1f8f9eae8bab31
david.coutu <david.coutu@e993bc6e-aa19-11dd-9a07-0f3041a8e97c>
2009-03-30 20:31:51 +0000

    Upgrade the version of several gems which had code impacts. Added features for submitting gestures and the start of the step definitions for them.

---
+++ r-warlocks/features/challenge_playing/step_definitions/gesture_steps.rb
@@ -0,0 +1,5 @@
+Given /^I am in a challenge with "([^\"]*)" that is in the started state$/ do |warlock|
+  Given "a warlock named \"#{warlock}\" exists"
+  warlock = session[:warlock]
+  Given "a challenge was created by \"#{warlock.name}\" for \"2\" players with \"#{warlock}\" joined"
+end
